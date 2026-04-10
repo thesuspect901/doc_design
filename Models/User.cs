dd753506d64ef505edcb8bc8d65cea389108d3e7
thesuspect901 <151927672+thesuspect901@users.noreply.github.com>
2026-04-10 21:42:42 +0300

    Lab3 MVC Discord

---
+++ Models/User.cs
@@ -0,0 +1,11 @@
+namespace lab3_doc_design.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string Username { get; set; }
+
+        public string Email { get; set; }
+    }
+}
